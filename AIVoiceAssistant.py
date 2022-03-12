c80cf8746abd61deaa440d1894bfe3ebc9dff356
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2022-03-12 12:51:20 -0500

    Add files via upload

--- AIVoiceAssistant.py
+++ AIVoiceAssistant.py
@@ -1,3 +1,4 @@
+from numpy import take
 import speech_recognition as sr
 import pyttsx3
 import datetime
@@ -7,7 +8,11 @@ import time
 import subprocess
 import wolframalpha
 import requests
-import json
+import pyjokes
+import pywhatkit
+import pyautogui
+import os
+import beepy
 
 print('Loading the AI Voice Assistant - Robostoa')
 
@@ -15,6 +20,29 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', 'voices[0].id')
 
+def alarm(h, m, s):
+
+    # Calculate the total number of seconds
+    total_seconds = h * 3600 + m * 60 + s
+
+    # While loop that checks if total_seconds reaches zero
+    # If not zero, decrement total time by one second
+    while total_seconds > 0:
+
+        # Timer represents time left on countdown
+        timer = datetime.timedelta(seconds=total_seconds)
+
+        # Prints the time left on the timer
+        print(timer, end="\r")
+
+        # Delays the program one second
+        time.sleep(1)
+
+        # Reduces total time by one second
+        total_seconds -= 1
+
+    print("Bzzzt! The countdown is at zero seconds!")
+    beepy.beep(5)
 
 def speak(text):
     engine.say(text)
@@ -38,7 +66,7 @@ def takeCommand():
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=3)
+        r.adjust_for_ambient_noise(source, duration=1)
         print("Listening...")
         audio = r.listen(source)
 
@@ -48,6 +76,7 @@ def takeCommand():
 
         except Exception as e:
             speak("Excuse me, can you say that again")
+            print("Excuse me, can you say that again")
             return "None"
         return statement
 
@@ -60,6 +89,7 @@ if __name__ == '__main__':
 
     while True:
         speak("Tell me how can I help you?")
+        print("Tell me how can I help you?")
         statement = takeCommand().lower()
         if statement == 0:
             continue
@@ -69,11 +99,13 @@ if __name__ == '__main__':
             print('your AI Voice Assistant Robostoa is shutting down, Good bye!')
             break
 
-        if 'wikipedia' in statement:
+        if 'tell me about' in statement:
             speak('Searching Wikipedia...')
+            print("Searching Wikipedia")
             statement = statement.replace("wikipedia", "")
-            results = wikipedia.summary(statement, sentences=3)
+            results = wikipedia.summary(statement, 1)
             speak("According to Wikipedia")
+            print("According to Wikipedia")
             print(results)
             speak(results)
 
@@ -93,7 +125,7 @@ if __name__ == '__main__':
             time.sleep(5)
 
         elif "weather" in statement:
-            api_key = ""
+            api_key = "7e7c1f0e94a0f88363b4a92203c20f60"
             base_url = "https://api.openweathermap.org/data/2.5/weather?"
             speak("whats the city name")
             city_name = takeCommand()
@@ -128,8 +160,8 @@ if __name__ == '__main__':
 
         elif 'who are you' in statement or 'what can you do' in statement:
             speak('I am Robostoa, the best AI Robot you will ever find. For now i am programmed to do tasks like'
-                  'opening youtube, google, gmail and stackoverflow, predict forecast and weather in different cities, search in wikipedia'
-                  'get top headline news from new york times and you can ask me computational or geographical questions too!')
+                  'opening youtube, google, gmail and stackoverflow, play some songs for you, predict forecast, and weather in different cities, search in wikipedia, tell you a joke,'
+                  'get the top headline news from new york times and you can ask me computational or geographical questions too!')
 
         elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
             speak("I was built by Jorge Eldis")
@@ -150,10 +182,10 @@ if __name__ == '__main__':
             webbrowser.open_new_tab(statement)
             time.sleep(5)
 
-        elif 'ask' in statement:
-            speak('I can answer to computational and geographical questions and what question do you want to ask now')
+        elif 'calculate' in statement:
+            speak('I can answer to computational and geographical questions, what question do you want to ask now?')
             question = takeCommand()
-            app_id = ""
+            app_id = "WKV645-PE6XL6URPQ"
             client = wolframalpha.Client(app_id)
             res = client.query(question)
             answer = next(res.results).text
@@ -164,5 +196,37 @@ if __name__ == '__main__':
             speak(
                 "Ok , your pc will log off in 10 sec make sure you exit from all applications")
             subprocess.call(["shutdown", "/l"])
+
+        elif 'joke' in statement:
+            speak(pyjokes.get_joke())
+
+        elif 'play' in statement:
+            song = statement.replace('play', '')
+            speak('playing ' + song)
+            pywhatkit.playonyt(song)
+
+        elif 'volume up' in statement:
+            pyautogui.press("volumeup")
+
+        elif 'volume down' in statement:
+            pyautogui.press("volumedown")
+
+        elif 'volume mute' in statement or 'shut up' in statement:
+            pyautogui.press("volumemute")
+
+        elif "open notepad" in statement:
+            npath = "C:\\Windows\\system32\\notepad.exe"
+            os.startfile(npath)
+            speak('Opening notepad for you')
+
+        elif "alarm" in statement:
+            speak('Please tell me how long do you want this alarm to be: ')
+            speak('Please specify hours: ')
+            h = takeCommand()
+            speak('Please specify minutes: ')
+            m = takeCommand()
+            speak('Please specify seconds: ')
+            s = takeCommand()
+            alarm(int(h), int(m), int(s))
 
 time.sleep(3)
